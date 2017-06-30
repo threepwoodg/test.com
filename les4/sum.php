e8e3c8f5395daf68f352e2fe8d3e27c0b8957f84
threepwoodg <ttnonick@gmail.com>
2017-06-30 23:00:11 +0300

    GET POST

--- les4/sum.php
+++ les4/sum.php
@@ -1,11 +1,4 @@
-<html>
-<head>
-	<title>Галерея изображений</title> </head>
-<body>
-<h2>Какое-то описание картинки</h2>
 <?php
-for($i = 1; $i <= 16; $i++)
-	echo "<a href = photo.php?id=$i >Картинка No$i</a><br/>";
 
 define('SUM', 1);
 define('MIN', 2);
@@ -59,18 +52,21 @@ function mathOperation($x, $y, $operation)
 if(isset($_POST['a']) && isset($_POST['b'])) $result = mathOperation($_POST['a'], $_POST['b'],$_POST['operation']);
 else
 	$result = "";
-
+	var_dump($_POST);
 ?>
-<form method="post">
-    <input type="text" name="a" />
-    <select name="operation">
-        <option value="1">+</option>
-        <option value="2">-</option>
-        <option value="3">/</option>
-        <option value="4">*</option>
-    </select>
-    <input type="text" name="b" /> <input type="submit" value="=" /> <?php echo $result; ?>
-
-
-</body>
-</html>
+	<html>
+	<head>
+		<title>Галерея изображений</title> </head>
+	<body>
+	<form method="post">
+		<input type="text" name="a" />
+		<select name="operation">
+			<option value="1">+</option>
+			<option value="2">-</option>
+			<option value="3">/</option>
+			<option value="4">*</option>
+		</select>
+		<input type="text" name="b" /> <input type="submit" value="=" /> <?php echo $result; ?>
+	</form>
+	</body>
+	</html>
