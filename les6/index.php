54e866b0fe06e29b11e76eaf9c77e5187f101d65
threepwoodg <ttnonick@gmail.com>
2017-07-18 21:52:35 +0300

    les 6 0.2

--- les6/index.php
+++ les6/index.php
@@ -8,11 +8,63 @@
 
 header("Content-type: text/html; charset=utf-8;");
 
-$f = fopen('./test.txt', 'r');
-$c = fgets($f);
+function getGallery($path){
+	
+	$arr_files = scandir($path);
+	foreach ($arr_files as $k => $v) {
+		$files = $v;
+		if (($files != '.')&&($files != '..')){
+			if ((strpos($files,'.png'))||(strpos($files,'.jpeg'))||(strpos($files,'.jpg'))) {
+				echo "<img src=./img/{$v} height=95>";
+				echo $v;
+			}
+		}
+	}
+}
 
-echo $c;
+function uploadFiles($file){
+	if ($file['name'] == '') {
+		echo "Error, file is empty!";
+		return;
+	}
+	if ((strpos($file['name'], '.png')) || (strpos($file['name'], '.jpeg')) || (strpos($file['name'], '.jpg'))) {
+		if (copy($file['tmp_name'], './img/' . $file['name']))
+			echo 'File upload!';
+		else
+			echo 'Error';
+	} else
+		echo "Error, select image!";
+}
 
-fclose($f);
+/*$f = fopen('./test.txt', 'r');
+	$c = file_get_contents('./test.txt');
+	echo $c . "<br>";
+	fclose($f);*/
 ?>
 
+<html>
+<body>
+<head>
+	<link
+		rel="stylesheet"
+		href="./css/style.css"
+		type="text/css" />
+	<title>Gallery</title>
+</head>
+<h2>Gallery</h2>
+	<?php
+	getGallery('./img');
+	?>
+<h3>Select and pull your image</h3>
+<form method="post" action="./index.php" enctype="multipart/form-data">
+	<input type="file" name="user_image"><br>
+	<input type="submit" value="send">
+</form>
+	<?php
+	
+	if (isset($_FILES['user_image'])){
+		uploadFiles($_FILES['user_image']);
+	}
+	?>
+</body>
+</html>
