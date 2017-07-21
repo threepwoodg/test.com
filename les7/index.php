a73f49309bb24ffb48f683a57e05b36e8fa08558
threepwoodg <ttnonick@gmail.com>
2017-07-21 23:18:40 +0300

    les 7 0.2

--- les7/index.php
+++ les7/index.php
@@ -8,11 +8,15 @@
 
 header("Content-type: text/html; charset=utf-8;");
 
-define('SQL_USERNAME','root');
-define('SQL_PASSWORD','root');
-define('SQL_HOST','localhost');
+define('SQL_USERNAME','rem_root');
+define('SQL_PASSWORD','rem_root');
+define('SQL_HOST','87.229.176.206');
 define('SQL_PORT','8889');
 define('SQL_DB','test_db');
+
+define('PNG','image/png');
+define('JPEG','image/jpeg');
+define('JPG','image/jpg');
 
 function getGallery($path){
 	
@@ -20,40 +24,34 @@ function getGallery($path){
 	foreach ($arr_files as $k => $v) {
 		$files = $v;
 		if (($files != '.')&&($files != '..')){
-			if ((strpos($files,'.png'))||(strpos($files,'.jpeg'))||(strpos($files,'.jpg'))) {
-				echo "<a href='./img/{$v}'><img src=./img/{$v} height=95></a>";
-				echo $v;
+				echo "<a href='./img/{$v}'><img src=./img/{$v} height=150></a>";
 			}
 		}
-	}
 }
 
 function uploadFiles($file){
 	if ($file['name'] == '') {
-		echo "Error, file is empty!";
+		echo 'Error, file is empty!';
 		return;
 	}
-	if ((strpos($file['name'], '.png')) || (strpos($file['name'], '.jpeg')) || (strpos($file['name'], '.jpg'))) {
-		if (copy($file['tmp_name'], './img/' . $file['name']))
-			echo 'File upload!';
-		else
-			echo 'Error';
-	} else
-		echo "Error, select image!";
+	if (($file['type'] == PNG)||($file['type'] == JPEG)||($file['type'] == JPG)) {
+			if (copy($file['tmp_name'], './img/' . $file['name']))
+				echo 'File upload!';
+			        else
+				        echo 'Error';
+		    } else
+			echo 'Error, select image!';
 }
 
 $link = mysqli_init();
-var_dump($link);
 
 $success = mysqli_real_connect($link, SQL_HOST, SQL_USERNAME, SQL_PASSWORD, SQL_DB, SQL_PORT);
 
 $query = mysqli_query($link,'SELECT * FROM `Users`');
 
-
 while ($row = mysqli_fetch_assoc($query)){
-    var_dump($row);
     foreach ($row as $k => $v){
-        echo "{$k} = {$v} <br>";
+        echo "{$k} - {$v} <br>";
     }
 }
 
@@ -73,9 +71,10 @@ while ($row = mysqli_fetch_assoc($query)){
 	getGallery('./img');
 	?>
 <h3>Select and pull your image</h3>
-<form method="post" action="./index.php" enctype="multipart/form-data">
+<form method="post" action="index.php" enctype="multipart/form-data">
 	<input type="file" name="user_image"><br>
-	<input type="submit" value="send">
+    <textarea name="description"></textarea>
+	<input type="submit" value="sand">
 </form>
 	<?php
 	
